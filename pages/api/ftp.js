0ed4c5c57f8d49e941014e809e10e2eb2742fc4e
Rene Rodriguez <rene.rrrg@gmail.com>
2020-07-07 19:31:03 +0200

    code update

--- pages/api/ftp.js
+++ pages/api/ftp.js
@@ -6,19 +6,22 @@ export default async (req, res) => {
     client.ftp.verbose = true
     try {
         await client.access({
-            host: "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt",
+            host: "aftp.cmdl.noaa.gov",
             port: 21,
-            secure: true
+            user: "anonymous",
+            password: "guest",
+            secure: false
         })
         console.log(await client.list())
-       await client.ensureDir("ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt")
-      
+        await client.downloadTo("warming-data.txt", "ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt");
     }
     catch(err) {
         console.log(err)
     }
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end("hola")
     client.close()
    
- 
   }
   
