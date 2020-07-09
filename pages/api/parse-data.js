f73f4bcc097c00fc876a3f2f752b7c1a7a7445e9
Rene Rodriguez <rene.rrrg@gmail.com>
2020-07-09 16:41:24 +0200

    still working on it

--- pages/api/parse-data.js
+++ pages/api/parse-data.js
@@ -3,30 +3,27 @@ var fs = require('fs'),
     filePath = path.join(__dirname, 'public/data/clear-data-1979.json');
 
 export default function (req, response) {
+    const array = [];
     fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-       
+     
         if (!err) {
             const parsedData = JSON.parse(data)
             delete parsedData[0];
             console.log(parsedData);
-            const array = [];
+            
             for (var i = 1; i < 1979; i++) {
                 const time = parsedData[i].split(" ")[0];
                 const temperature = parsedData[i].split(" ")[3] ? parsedData[i].split(" ")[3] : parsedData[i].split(" ")[4];
                 console.log(temperature)
                 const land = temperature.slice(0, 5)
                 array[i] = { time, land }
-                console.log(array[i])
-
             }
-            const obj = { test: "object" }
-            response.setHeader('Cache-control', "s-maxage=86400");
-            response.status(200).json(obj);
-        
+          
         } else {
             console.log(err);
-
         }
     });
-
+   
+    response.setHeader('Content-Type', 'application/json');
+    response.status(200).json({error: null, array});
 }
