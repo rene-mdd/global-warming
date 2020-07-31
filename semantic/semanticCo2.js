c5be1a0a9004c24d674f8649000ac16c9414975b
Rene Rodriguez <rene.rrrg@gmail.com>
2020-07-31 16:59:42 +0200

    responsive working. Test preview picture

--- semantic/semanticCo2.js
+++ semantic/semanticCo2.js
@@ -52,23 +52,23 @@ class SemanticCo2 extends React.Component {
            {this.state.co2 ? <Co2 callBackPropCo2={(c) => { this.handleClickCo2(c)}} /> : null }
         </Grid.Row>
         <Grid.Row centered={true}>
-            <Grid.Column width="three" textAlign="center">
+            <Grid.Column width="eight" textAlign="center">
     <Button onClick={this.toggleCo2} className={this.state.co2Loading} id={this.state.co2Loading}>
         {this.state.co2 ? "Hide graph" : "Deploy graph"}
     </Button>
             </Grid.Column>
         </Grid.Row>
         </Grid>
-   <Grid columns="equal" >
+   <Grid columns="equal">
        <Container textAlign="justified">
        {/* <Observer {...options}> */}
        <p>For thousands of years, the natural concentration of CO2 in earth atmosphere was around 280 ppm. From the beginning of the industrial revolution, human activities like the burning of fossil fuels, deforestation, and livestock have increased this amount by more than 30%.
       For more information about prehistoric CO2 concentration, current and future consequences please visit <a href="https://www.climate.gov/news-features/understanding-climate/climate-change-atmospheric-carbon-dioxide" target="_blank">source</a> and <a href="https://climate.nasa.gov/climate_resources/24/graphic-the-relentless-rise-of-carbon-dioxide/" target="_blank">source</a>.</p>
       {/* </Observer> */}
-            <Grid columns="equal" className="api-segment">
-                <Grid.Column><AccordionCo2 /></Grid.Column>
+            <Grid className="api-segment" columns="equal" centered stackable>
+                <Grid.Column><AccordionCo2/></Grid.Column>
                 <Grid.Column><AccordionShare/></Grid.Column>
-            </Grid>
+                </Grid>
        </Container>
    </Grid>
     
--- semantic/semanticMethane.js
+++ semantic/semanticMethane.js
@@ -39,7 +39,7 @@ class SemanticMethane extends React.Component {
            {this.state.methane ? <Methane callBackPropMethane={(c) => { this.handleClickMethane(c)}} /> : null }
         </Grid.Row>
         <Grid.Row centered={true}>
-            <Grid.Column width="three" textAlign="center">
+            <Grid.Column width="eight" textAlign="center">
     <Button onClick={this.toggleMethane} className={this.state.methaneLoading} id={this.state.methaneLoading}>
         {this.state.methane ? "Hide graph" : "Deploy graph"}
     </Button>
@@ -50,10 +50,10 @@ class SemanticMethane extends React.Component {
        <Container textAlign="justified">
        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <a href="https://www.climate.gov/news-features/understanding-climate/climate-change-atmospheric-carbon-dioxide" target="_blank">source</a> and <a href="https://climate.nasa.gov/climate_resources/24/graphic-the-relentless-rise-of-carbon-dioxide/" target="_blank">source</a>.
             </p>
-            <Grid columns="equal" className="api-segment">
-                <Grid.Column><AccordionMethane /></Grid.Column>
-                <Grid.Column><AccordionShare /></Grid.Column>
-            </Grid>
+            <Grid className="api-segment" columns="equal" centered stackable>
+                <Grid.Column><AccordionMethane/></Grid.Column>
+                <Grid.Column><AccordionShare/></Grid.Column>
+                </Grid>
        </Container>
    </Grid>
     
