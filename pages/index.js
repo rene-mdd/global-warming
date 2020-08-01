b01722833e2f89f9e0e5ce356a2a9c258e21349c
Rene Rodriguez <rene.rrrg@gmail.com>
2020-08-01 13:33:11 +0200

    preview ready. setting it up

--- pages/index.js
+++ pages/index.js
@@ -99,12 +99,10 @@ class Home extends React.Component {
       {/* <html prefix="og: https://ogp.me/ns#"></html> */}
       <meta property="og:url" content={"https://global-warming-azure.vercel.app/api/chart-img/tempChart.jpeg"} />
       <meta property="og:title" content={"global Warming data"} />
-      <meta property="og:description" content={"this site contains data about gw"} />
       <meta property="og:image" content={`api/chart-img/tempChart.jpeg`} />
-      <meta property="og:image:width" content="200px" />
-      <meta property="og:image:height" content="200px" />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta name="description" content="Global Warming and Climate Change live API, data, graphs, and news." />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="path/to/chartjs/dist/Chart.min.css" />
         <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
