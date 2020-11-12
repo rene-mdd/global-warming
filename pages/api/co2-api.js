ee540a61664cb785b08ce13b8eba1b8119a3f254
Rene Rodriguez <rene.rrrg@gmail.com>
2020-11-12 15:42:14 +0100

    cors added directly to file

--- pages/api/co2-api.js
+++ pages/api/co2-api.js
@@ -54,7 +54,15 @@ export default async (req, res) => {
       //  date.push(`${obj.year}.${obj.field2}.${obj.field3}`);
       //  co2.push(parseFloat(obj.field4))
     })
-
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     res.statusCode = 200
     res.setHeader('Cache-Control', 's-maxage=43200')
     res.json({ co2 })
