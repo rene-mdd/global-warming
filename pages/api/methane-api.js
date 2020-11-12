82417307a7e31829a08b197c15bfc019cbd0a4f8
Rene Rodriguez <rene.rrrg@gmail.com>
2020-11-12 15:51:10 +0100

    only get added to co2

--- pages/api/methane-api.js
+++ pages/api/methane-api.js
@@ -4,87 +4,91 @@ const csv = require('csvtojson')
 // `link: ftp://aftp.cmdl.noaa.gov/products/trends/ch4/ch4_mm_gl.txt`
 
 export default async (req, res) => {
-  const connectionConfig = {
-    host: 'aftp.cmdl.noaa.gov',
-    connTimeout: 30000,
-    user: 'anonymous',
-    password: 'guest',
-    secure: false
-  }
+    const connectionConfig = {
+        host: 'aftp.cmdl.noaa.gov',
+        connTimeout: 30000,
+        user: 'anonymous',
+        password: 'guest',
+        secure: false
+    }
 
-  const { data, error } = await getFTPData(
-    connectionConfig,
-    'products/trends/ch4/ch4_mm_gl.txt'
-  )
+    const {data, error} = await getFTPData(connectionConfig, 'products/trends/ch4/ch4_mm_gl.txt')
 
-  if (error) {
-    res.statusCode = 500
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Internal Server Error')
-    return
-  }
+    if (error) {
+        res.statusCode = 500
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('Internal Server Error')
+        return
+    }
 
-  csv()
-    .fromString(data)
-    .then(jsonObj => {
-      parsedData(jsonObj)
+    csv().fromString(data).then(jsonObj => {
+        parsedData(jsonObj)
     })
 
-  function parsedData (csvToJson) {
-    const methaneData = []
-    const oldKey =
-      '# --------------------------------------------------------------------'
-    const sliced = csvToJson.slice(62)
-    sliced.forEach(obj => {
-      if (oldKey !== 'year') {
-        Object.defineProperty(
-          obj,
-          ['year'],
-          Object.getOwnPropertyDescriptor(obj, oldKey)
-        )
-        delete obj[oldKey]
-      }
-      methaneData.push({
-        date: `${obj.year.split(' ').filter(f => f)[0]}.${
-          obj.year.split(' ').filter(f => f)[1]
-        }`,
-        average: `${obj.year.split(' ').filter(f => f)[3]}`,
-        trend: `${obj.year.split(' ').filter(f => f)[5]}`,
-        averageUnc: `${obj.year.split(' ').filter(f => f)[4]}`,
-        trendUnc: `${obj.year.split(' ').filter(f => f)[6]}`
-      })
-    })
-    res.statusCode = 200
-    res.setHeader('Cache-Control', 's-maxage=43200')
-    res.json({ methane: methaneData })
-    return
-  }
+    function parsedData(csvToJson) {
+        const methaneData = []
+        const oldKey = '# --------------------------------------------------------------------'
+        const sliced = csvToJson.slice(62)
+        sliced.forEach(obj => {
+            if (oldKey !== 'year') {
+                Object.defineProperty(obj, ['year'], Object.getOwnPropertyDescriptor(obj, oldKey))
+                delete obj[oldKey]
+            }
+            methaneData.push({
+                date: `${
+                    obj.year.split(' ').filter(f => f)[0]
+                }.${
+                    obj.year.split(' ').filter(f => f)[1]
+                }`,
+                average: `${
+                    obj.year.split(' ').filter(f => f)[3]
+                }`,
+                trend: `${
+                    obj.year.split(' ').filter(f => f)[5]
+                }`,
+                averageUnc: `${
+                    obj.year.split(' ').filter(f => f)[4]
+                }`,
+                trendUnc: `${
+                    obj.year.split(' ').filter(f => f)[6]
+                }`
+            })
+        })
+        // cors config
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+        res.statusCode = 200
+        res.setHeader('Cache-Control', 's-maxage=43200')
+        res.json({methane: methaneData})
+        return
+    }
 }
 
-const getFTPData = (config, path) => {
-  // Creating FTP Client
-  const ftp = new Client()
+const getFTPData = (config, path) => { // Creating FTP Client
+    const ftp = new Client()
 
-  // Register our events ("read")
-  const getFile = new Promise((resolve, _) => {
-    let data = ''
+    // Register our events ("read")
+    const getFile = new Promise((resolve, _) => {
+        let data = ''
 
-    ftp.on('ready', function () {
-      ftp.get(path, (error, stream) => {
-        if (error) {
-          resolve({ data: null, error: error })
-        }
-        // The file comes as a stream (in pieces) we add each piece to data
-        stream.on('data', chunk => (data += chunk.toString()))
-        // Then we got all pieces we can resolve the promise
-        stream.on('end', () => resolve({ data, error: null }))
-      })
+        ftp.on('ready', function () {
+            ftp.get(path, (error, stream) => {
+                if (error) {
+                    resolve({data: null, error: error})
+                }
+                // The file comes as a stream (in pieces) we add each piece to data
+                stream.on('data', chunk => (data += chunk.toString()))
+                // Then we got all pieces we can resolve the promise
+                stream.on('end', () => resolve({data, error: null}))
+            })
+        })
     })
-  })
 
-  // We need to connect after we registered our events
-  ftp.connect(config)
+    // We need to connect after we registered our events
+    ftp.connect(config)
 
-  // we return our promise aka when we got all file pieces
-  return getFile
+    // we return our promise aka when we got all file pieces
+    return getFile
 }
--- pages/api/temperature-api.js
+++ pages/api/temperature-api.js
@@ -2,33 +2,31 @@ import axios from 'axios'
 
 const line2Array = line => line.split(' ').filter(ele => ele)
 
-const convertToObject = valueArray2d =>
-  valueArray2d.map(([time, station, land]) => {
-    return {
-      time,
-      station,
-      land
-    }
-  })
+const convertToObject = valueArray2d => valueArray2d.map(([time, station, land]) => {
+    return {time, station, land}
+})
 
 export default async (req, res) => {
-  try {
-    const { data } = await axios.get(
-      'https://data.giss.nasa.gov/gistemp/graphs_v4/graph_data/Monthly_Mean_Global_Surface_Temperature/graph.txt'
-    )
+    try {
+        const {data} = await axios.get('https://data.giss.nasa.gov/gistemp/graphs_v4/graph_data/Monthly_Mean_Global_Surface_Temperature/graph.txt')
 
-    const lines = data.split('\n')
-    // in the _ vars we store the dashed lines
-    const [note, _, heading, __, ...stats] = lines
+        const lines = data.split('\n')
+        // in the _ vars we store the dashed lines
+        const [note, _, heading, __, ...stats] = lines
 
-    // map over each line and remove the white space that separates the values
-    const dataAs2dArray = stats.map(line2Array).filter(ele => ele.length)
+        // map over each line and remove the white space that separates the values
+        const dataAs2dArray = stats.map(line2Array).filter(ele => ele.length)
 
-    const result = convertToObject(dataAs2dArray)
-    // caching the response for one day (just max one slow request per day)
-    res.setHeader('Cache-Control', 's-maxage=43200')
-    res.status(200).json({ error: null, result })
-  } catch (error) {
-    res.status(500).send({ result: null, error })
-  }
+        const result = convertToObject(dataAs2dArray)
+        // cors config
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version')
+        // caching the response for one day (just max one slow request per day)
+        res.setHeader('Cache-Control', 's-maxage=43200')
+        res.status(200).json({error: null, result})
+    } catch (error) {
+        res.status(500).send({result: null, error})
+    }
 }
