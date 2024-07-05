72bee6face72d7787eb104d385097e4db0ba8f6e
rene-mdd <rene.rrrg@gmail.com>
2024-07-05 11:19:11 +0200

    fetch exchanged for axios

--- pages/api/temperature-api.js
+++ pages/api/temperature-api.js
@@ -7,20 +7,24 @@ const convertToObject = (valueArray2d) =>
 
 export default async (req, res) => {
   try {
-    const data = await fetch(
-      "https://data.giss.nasa.gov/gistemp/graphs_v4/graph_data/Monthly_Mean_Global_Surface_Temperature/graph.txt"
-    );
+    const response = await fetch(
+      "https://data.giss.nasa.gov/gistemp/graphs_v4/graph_data/Monthly_Mean_Global_Surface_Temperature/graph.txt", {
+        headers: {
+          'Accept': "text/plain",
+        }
+      });
+      const data = await response.text();
 
-    // const lines = data.split("\n");
-    // // in the _ vars we store the dashed lines
-    // // eslint-disable-next-line no-unused-vars
-    // const [note, _, heading, __, ...stats] = lines;
+    const lines = data.split("\n");
+    // in the _ vars we store the dashed lines
+    // eslint-disable-next-line no-unused-vars
+    const [note, _, heading, __, ...stats] = lines;
 
-    // // map over each line and remove the white space that separates the values
-    // const dataAs2dArray = stats.map(line2Array).filter((ele) => ele.length);
+    // map over each line and remove the white space that separates the values
+    const dataAs2dArray = stats.map(line2Array).filter((ele) => ele.length);
 
-    // const result = convertToObject(dataAs2dArray);
-    // // cors config
+    const result = convertToObject(dataAs2dArray);
+    // cors config
     res.setHeader("Access-Control-Allow-Credentials", true);
     res.setHeader("Content-Type", "application/json");
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -30,12 +34,12 @@ export default async (req, res) => {
       "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
     );
     // caching the response for 12 hours day (just max one slow request per day)
-    res.setHeader('Vercel-CDN-Cache-Control', 'public, max-age=0. s-maxage=86400, stale-while-revalidate=3600');
-    res.setHeader('CDN-Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate=3600');
-    res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate=3600');
-    res.status(200).json({ error: null, data });
-
+    res.setHeader('Vercel-CDN-Cache-Control', 'public, max-age=3600. s-maxage=43200, stale-while-revalidate=3600');
+    res.setHeader('CDN-Cache-Control', 'public, max-age=3600, s-maxage=43200, stale-while-revalidate=3600');
+    res.setHeader('Cache-Control', 'public, max-age=3600, s-maxage=43200, stale-while-revalidate=3600');
+    res.status(200).json({ error: null, result });
+    
   } catch (error) {
-    res.status(500).send({ result: data, error });
+    res.status(500).send({ result: null, error });
   }
 };
