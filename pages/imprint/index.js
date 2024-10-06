f18472231a47ba23b9965b01d60aff8a43bf88a6
rene-mdd <rene.rrrg@gmail.com>
2024-10-06 23:23:28 +0100

    privacy policy and imprint added

--- pages/imprint/index.js
+++ pages/imprint/index.js
@@ -1,15 +1,25 @@
-import { Link, Typography } from "@mui/material";
+import { Link, Popper, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Container } from "@mui/system";
+import { Box, Container } from "@mui/system";
+import { useState } from "react";
 import StickyMenu from "../../components/semantic/menu";
 import SiteHeader from "../../components/siteHeader";
 import Footer from "../../components/semantic/footer";
 
+
 function Imprint() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popper' : undefined;
 
   const imprintTitle = "Climate Accountability API - Imprint";
   const imprintMetaDescription =
-    "Climate Accountability API gUG. Berlin, Germany.";
+    "Imprint: Climate Accountability API gUG. Berlin, Germany.";
   const imprintKeywords =
     "Global warming, about, carbon footprint, climate change, environment";
   return (
@@ -22,20 +32,19 @@ function Imprint() {
       <StickyMenu />
       <Grid
         container
-
       >
         <Container>
           <Typography component="h2" align="left" className="about-title" mb={5}>
-            Imprint
+            Imprint / Site Notice
           </Typography>
           <Typography component="p" mb={4}>
-            Climate Accountability API
+            Climate Accountability API gUG (haftungsbeschränkt)
           </Typography>
           <Typography component="p">
-            Seestraße 107,
+            Seestraße 107
           </Typography>
           <Typography component="p">
-            13353 Berlin,
+            13353 Berlin
           </Typography>
           <Typography component="p" mb={4}>
             Deutschland
@@ -44,29 +53,31 @@ function Imprint() {
             <Typography component="p">
               Email: <Typography component="span">info@global-warming.org</Typography>
             </Typography>
+            <Typography component="div">
+              Phone: <button aria-describedby={id} type="button" onClick={handleClick}>Show number</button>
+              <Popper id={id} open={open} anchorEl={anchorEl}>
+                <Box sx={{ border: 1, p: 1, bgcolor: 'white' }}>
+                  0 &#49;&#53;&#50;&#48; &#50;&#52;&#49;&#56;&#51;&#48;&#49;
+                </Box>
+              </Popper>
+            </Typography>
             <Typography component="p" mb={4}>
               Contact form: <Link href="/contact">Contact</Link>
             </Typography>
             <Typography component="p">
+              Commercial Register: <Typography component="span">HRB 262202 B</Typography>
+            </Typography>
+            <Typography component="p">
               Register: <Typography component="span">Handelsregister</Typography>
             </Typography>
-            <Typography component="p">
-              Registernummer: <Typography component="span">HRB 262202 B</Typography>
-            </Typography>
             <Typography component="p" mb={4}>
-              Registergericht: <Typography component="span">Charlottenburg</Typography>
+              Registration court: <Typography component="span">Amtsgericht Charlottenburg</Typography>
             </Typography>
             <Typography component="p">
-              UID-Nummer: <Typography component="span">DE123456789</Typography>
-            </Typography>
-            <Typography component="p">
-              Professional title: <Typography component="span">Software development, Data engineering</Typography>
+              VAT ID: <Typography component="span">Sales tax identification number according to Sect. 27 a of the Sales Tax Law: DE999999999</Typography>
             </Typography>
             <Typography component="p" mb={4}>
-              Awarding country: <Typography component="span">Germany</Typography>
-            </Typography>
-            <Typography component="p" mb={4}>
-              Managing director: <Typography component="span">René Rodríguez</Typography>
+              Represented by: <Typography component="span">René Rodríguez</Typography>
             </Typography>
             <Typography component="p" mb={5}>
               Imprint of the data protection authority: <Typography component="span">https://global-warming.org/imprint</Typography>
@@ -85,11 +96,21 @@ function Imprint() {
               <Link href="https://www.universalschlichtungsstelle.de/"> (www.universalschlichtungsstelle.de)</Link>
             </Typography>
             <Typography component="h3" mt={4} mb={4}>
-              EU-Streitschlichtung
+              EU dispute resolution
             </Typography>
             <Typography component="p" mb={5}>
-              In the event of a dispute, consumers have the option to use the European Online Dispute Resolution (ODR) platform. You can access the platform at the following link:
-              <Link href="https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home.chooseLanguage"> (EU Online Dispute Resolution Platform)</Link>
+              The European Commission provides a platform for online dispute resolution (ODR):
+              <Link href="https://ec.europa.eu/consumers/odr/"> (https://ec.europa.eu/consumers/odr/)</Link>
+              Our e-mail address can be found above in the site notice.
+            </Typography>
+            <Typography component="h3" mt={4} mb={4}>
+              Dispute resolution proceedings in front of a consumer arbitration
+              board
+            </Typography>
+            <Typography component="p" mb={5}>
+              We participate in a dispute settlement procedure before a consumer arbitration board. The competent
+              consumer arbitration board is Zentrum für Schlichtung e.V., Straßburger Straße 8, 77694 Kehl am Rhein:
+              <Link href="https://www.verbraucher-schlichter.de"> (https://www.verbraucher-schlichter.de).</Link>
             </Typography>
           </Container>
         </Container>
